30b562c66c6570ef1efa335499ddc53abb2b66d2
PHAM HUONG LY <huongly2003.hn@gmail.com>
2025-06-10 17:24:34 +0700

    Initial commit

---
+++ src/main/java/com/finai/finance_be/FinanceBeApplication.java
@@ -0,0 +1,13 @@
+package com.finai.finance_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finai/finance_be/FinanceBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finai.finance_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
